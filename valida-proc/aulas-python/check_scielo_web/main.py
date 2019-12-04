8b499bc91f9bfbed15bdab9360915bb5992ac6cf
Franklin Ribeiro <franklin.ribeiro@scielo.org>
2019-12-04 18:14:22 -0300

    aula 5

--- valida-proc/aulas-python/check_scielo_web/main.py
+++ valida-proc/aulas-python/check_scielo_web/main.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import requests
+from lxml import html
 import re
 
 """
@@ -31,12 +32,27 @@ if __name__ == "__main__":
 
     # Ler o arquivo Scilista
 
-    fp = open("./valida-proc/aulas-python/check_scielo_web/scilista.lst", 'r')
+    fp = open("/Users/franklin.ribeiro/Documents/Python/valida-proc/aulas-python/check_scielo_web/scilista.lst", 'r')
+
+    issns = []
+    j_not_found = []
 
     for line in fp.readlines():
         acron, issue_label = line.split()
         ret = requests.get("%s%s/%s" % (PROTOCOL, DOMAIN, acron))
         if ret.status_code == 200:
             issn = re.search(REGEX_ISSN, ret.url)
-            print(issn.group())
+            issns.append(issn.group())
+        else:
+            j_not_found.append(issn.group())
+    for issn in issns:
+        url_segment = URL_SEGMENT % (SCRIPT_URL['grid'], issn)
+        ret = requests.get("%s%s/%s" % (PROTOCOL, DOMAIN, url_segment))
+
+        tree = html.fromstring(ret.content)
+        table = tree.xpath('//table')[3]
+        print("Conteúdo da tablela fasciculo: %s \n" % table.text_content)
+        
+
+
     print("teste")
